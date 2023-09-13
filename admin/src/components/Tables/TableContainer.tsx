30239e222dc5d62ff17c012f3829230f26b9850e
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-09-13 17:46:22 +0300

    admin: feat: table pagination

--- admin/src/components/Tables/TableContainer.tsx
+++ admin/src/components/Tables/TableContainer.tsx
@@ -1,6 +1,7 @@
-import { /* useEffect,*/ useState } from "react";
+import { /* useEffect,*/ useEffect, useState } from "react";
 import TableTemplate, { TableData } from "./TableTempalte";
 import TableSearch from "./TableSearch";
+import TablePagination from "./TablePagination";
 // import TableFilters from "./TableFilters";
 import tableFilter from "../../libs/tableHeplers/tableFilter";
 import { SortOrder } from "./TableSort";
@@ -10,7 +11,7 @@ import tableSort from "../../libs/tableHeplers/tableSort";
  * TODO
  * 1. Filter buttons and reset button
  * 2. Choose which columns to show
- * 3. Pagination
+ * 3. Pagination - Done
  * 4. Make table editable
  * 5. create real table template where dev can choose which features he needs, how to display data, modify rows
  */
@@ -18,23 +19,59 @@ import tableSort from "../../libs/tableHeplers/tableSort";
 const TableContainer = ({ data }: { data: TableData }) => {
   const [viewData, setViewData] = useState(data);
 
+  //Pagination
+  const [currentPageData, setCurrentPageData] = useState<TableData>({
+    columnsNames: [],
+    rowsData: [],
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const getCurrentPageData = () => {
+    let firstItem = (currentPage - 1) * itemsPerPage;
+    let lastItem = firstItem + itemsPerPage;
+    const currentPageRowsData = viewData.rowsData.slice(firstItem, lastItem);
+
+    const currentPageData: TableData = {
+      columnsNames: data.columnsNames,
+      rowsData: currentPageRowsData,
+    };
+
+    setCurrentPageData(currentPageData);
+  };
+
+  const getMaxPage = () => {
+    return Math.ceil(viewData.rowsData.length / itemsPerPage);
+  };
+
   const handleSearchText = (searchText: string) => {
     if (searchText === "" || searchText === null) setViewData(data);
-    console.info("render");
+    console.info("Render handle Search text");
     setViewData(tableFilter(data, searchText));
   };
 
   const handleSort = (i: number, order: SortOrder) => {
     if (order === "") return;
-
+    console.info("Render handle sort");
     setViewData(tableSort(data, i, order));
   };
+
+  useEffect(() => {
+    getCurrentPageData();
+    console.log("updated current page data");
+  }, [itemsPerPage, currentPage, viewData]);
 
   return (
     <div>
       <TableSearch onSearch={handleSearchText} />
       {/* <TableFilters onSearch={handleSearchText} /> */}
-      <TableTemplate data={viewData} tableSort={handleSort} />
+      <TableTemplate data={currentPageData} tableSort={handleSort} />
+      <TablePagination
+        onChangeItemsPerPage={setItemsPerPage}
+        maxPage={getMaxPage()}
+        onChangePage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
--- admin/src/components/pages/OrdersPage.tsx
+++ admin/src/components/pages/OrdersPage.tsx
@@ -4,7 +4,7 @@
     2.Change status abitity. Status enum.
     3.OrderPage.
     4.Filtering orders by ... status
-    5.Sorting orders by each column
+    5.Sorting orders by each column - Done
     6.Pagination with load more
     7.Some additional status to order like important or immediate
 
@@ -84,6 +84,747 @@ const ordersJson = `
   },
   {
     "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },{
+    "order_id": "005",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "006",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "007",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },
+  {
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },{
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  }, {
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },{
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },
+  {
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },{
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },
+   {
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },{
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },
+  {
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "003",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "004",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "success"
+  },{
+    "order_id": "001",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "new"
+  },
+  {
+    "order_id": "002",
+    "customer_name": "Jane Smith",
+    "products": [
+      {
+        "product_id": "1003",
+        "product_name": "Thingamajig",
+        "quantity": 5,
+        "price": 7.49
+      }
+    ],
+    "order_date": "2023-09-06",
+    "order_status": "sent"
+  },
+  {
+    "order_id": "047",
+    "customer_name": "John Doe",
+    "products": [
+      {
+        "product_id": "1001",
+        "product_name": "Widget",
+        "quantity": 3,
+        "price": 10.99
+      },
+      {
+        "product_id": "1002",
+        "product_name": "Gadget",
+        "quantity": 2,
+        "price": 19.99
+      }
+    ],
+    "order_date": "2023-09-07",
+    "order_status": "canceled"
+  },
+  {
+    "order_id": "048",
     "customer_name": "Jane Smith",
     "products": [
       {
