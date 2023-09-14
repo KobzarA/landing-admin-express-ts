2b38f3da9eb89bccec5d351574dd474898818735
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-09-14 17:33:37 +0300

    admin:feat filter buttons component

--- admin/src/components/Tables/TableTempalte.tsx
+++ admin/src/components/Tables/TableTempalte.tsx
@@ -5,37 +5,53 @@ export interface TableData {
   rowsData: (string | number | [] | object)[][];
 }
 
-const TableTemplate = ({
-  data,
-  tableSort,
-}: {
+export interface TableTemplateProps {
   data: TableData;
   tableSort: (i: number, order: "asc" | "dsc" | "") => void;
-}) => {
+  options?: {
+    sortable?: boolean;
+    editable?: boolean;
+    selectColumn?: boolean;
+  };
+}
+
+const TableTemplate = ({ data, tableSort, options }: TableTemplateProps) => {
   // Check for incoming data
   if (data.columnsNames.length === 0) return <div>"No data is downloaded"</div>;
 
   console.log("Render Table Template");
 
   const { columnsNames, rowsData } = data;
+  //
+  //checking for options
+  //
+  const sorterElement = (i: number) => {
+    //Checking if there data rowsData & and conditional render of sort comp
+    const elem = rowsData.length ? (
+      typeof rowsData[0][i] !== "object" ? (
+        <TableSort i={i} tableSort={tableSort} />
+      ) : null
+    ) : null;
+
+    return elem;
+  };
+
+  //
   // create table headers with conditional render of Table sort Component
+  //
   const columns = columnsNames.map((item, i) => {
     return (
       <th key={i} className=" border border-slate-600">
         <div className="mx-1 flex min-w-fit justify-between p-3">
           {item}
-          {/* Checking if there data rowsData & and conditional render of sort comp */}
-          {rowsData.length ? (
-            typeof rowsData[0][i] !== "object" ? (
-              <TableSort i={i} tableSort={tableSort} />
-            ) : null
-          ) : null}
+          {options?.sortable ? sorterElement(i) : null}
         </div>
       </th>
     );
   });
-
+  //
   //Make rows JSX[]
+  //
   const tdStyles = "border border-slate-700 p-4  table-cell ";
   const rows = rowsData.map((rowData, rowIndex) => {
     const row = rowData.map((data, columnIndex) => {
@@ -44,9 +60,13 @@ const TableTemplate = ({
         return (
           <td key={columnIndex} className={tdStyles} data-column={columnIndex}>
             <ol className="ml-3 list-decimal">
+              {/* Beta Render of list */}
               {data.map((item, i) => (
                 <li key={i}>
-                  {item?.product_name} - {item?.quantity} pcs.
+                  {/* {item?.product_name} - {item?.quantity} pcs. */}
+                  {typeof item === "object"
+                    ? Object.values<string>(item).join(" - ")
+                    : item}
                 </li>
               ))}
             </ol>
