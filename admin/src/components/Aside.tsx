2a07a595bf651349621a307840faa41f786095dc
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-09-19 23:59:51 +0300

    admin:feat: table change row components as modal

--- admin/src/components/Aside.tsx
+++ admin/src/components/Aside.tsx
@@ -12,12 +12,34 @@ const Aside = () => {
     <aside className="min-h-full border-r border-zinc-700 p-4">
       <h2 className="text-center text-lg">Navigation</h2>
       <nav className="flex flex-col items-start  ">
-        <ul className="ml-6 divide-y-2 divide-cyan-800 dark:divide-cyan-800">
+        <ul className="ml-6 space-y-3 divide-y-2 divide-cyan-800 dark:divide-cyan-800">
           <li>
-            <NavLink to={"/"}>Main page</NavLink>
+            <NavLink
+              to={"/"}
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "text-cyan-300"
+                  : isActive
+                  ? "text-cyan-500"
+                  : " hover:text-cyan-300"
+              }
+            >
+              Main page
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/orders"}>Orders</NavLink>
+            <NavLink
+              to={"/orders"}
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "text-cyan-300"
+                  : isActive
+                  ? "text-cyan-500"
+                  : "hover:text-cyan-300"
+              }
+            >
+              Orders
+            </NavLink>
           </li>
         </ul>
       </nav>
