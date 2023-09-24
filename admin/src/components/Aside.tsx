8e57e81daa8250df21776cd6563e082c6c66dcfb
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-09-24 10:15:43 +0300

    admin:feat Navlink styles basing on state, Global TODO, prepared lib helpers for upgading functionalllity

--- admin/src/components/Aside.tsx
+++ admin/src/components/Aside.tsx
@@ -7,6 +7,20 @@ import { NavLink } from "react-router-dom";
     , and be presented as icons?
     
 */
+
+function getNavlinkclasses({
+  isActive,
+  isPending,
+}: {
+  isActive: boolean;
+  isPending: boolean;
+}) {
+  return isPending
+    ? "text-cyan-300"
+    : isActive
+    ? "text-cyan-500"
+    : " hover:text-cyan-300";
+}
 const Aside = () => {
   return (
     <aside className="min-h-full border-r border-zinc-700 p-4">
@@ -14,30 +28,12 @@ const Aside = () => {
       <nav className="flex flex-col items-start  ">
         <ul className="ml-6 space-y-3 divide-y-2 divide-cyan-800 dark:divide-cyan-800">
           <li>
-            <NavLink
-              to={"/"}
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "text-cyan-300"
-                  : isActive
-                  ? "text-cyan-500"
-                  : " hover:text-cyan-300"
-              }
-            >
+            <NavLink to={"/"} className={getNavlinkclasses}>
               Main page
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to={"/orders"}
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "text-cyan-300"
-                  : isActive
-                  ? "text-cyan-500"
-                  : "hover:text-cyan-300"
-              }
-            >
+            <NavLink to={"/orders"} className={getNavlinkclasses}>
               Orders
             </NavLink>
           </li>
