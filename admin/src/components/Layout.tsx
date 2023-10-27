12bf1b0f1296f6c0bc39504294553726cf322147
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-10-27 15:14:03 +0300

    admin: changed main layout, show error message while auth

--- admin/src/components/Layout.tsx
+++ admin/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from "react";
 
 import Header from "./Header/Header";
 import Aside from "./Aside";
@@ -14,14 +14,12 @@ const Layout = ({
   setUser: Dispatch<SetStateAction<null | IUser>>;
 }) => {
   return (
-    <div className=" max-h-full min-h-screen">
+    <div className="grid-cols-main-layout grid-rows-main-layout grid">
+      <Aside />
       <Header user={user} setUser={setUser} />
-      <div className="flex min-h-full">
-        <Aside />
-        <main className="min-h-full border-l border-zinc-700 p-4">
-          <Outlet />
-        </main>
-      </div>
+      <main className="p-4">
+        <Outlet />
+      </main>
     </div>
   );
 };
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "main-layout": "minmax(0, 200px) minmax(0, 1fr)",
+      },
+      gridTemplateRows: {
+        "main-layout": "4rem minmax(0, 1fr)",
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s ease",
+        "fade-out": "fade-out 0.5s ease ",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        "fade-out": {
+          "0%": {
+            opacity: 1,
+          },
+          "100%": {
+            opacity: 0,
+          },
+        },
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
