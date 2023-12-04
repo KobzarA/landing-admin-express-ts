f9501d2350a0f7eba50d5b7d608baadec2d18df2
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-12-04 07:50:36 +0200

    admin:backArrow button moved as reuseble component

--- admin/src/components/pages/UsersCreatePage.tsx
+++ admin/src/components/pages/UsersCreatePage.tsx
@@ -1,30 +1,15 @@
 import UserCreateForm from "../Users/UserCreateForm";
-import { Link } from "react-router-dom";
 import UserCreateForm2 from "../Users/UserCreateForm2";
+import BackArrowButton from "../NavElements/BackArrowButton";
 
 const UsersCreatePage = () => {
   return (
     <>
       <div>
-        <Link to={".."} relative="path">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </Link>
+        <BackArrowButton />
       </div>
       <div className=" mx-auto max-w-xl">
-        <UserCreateForm />
+        {/* <UserCreateForm /> */}
         <UserCreateForm2 />
       </div>
     </>
